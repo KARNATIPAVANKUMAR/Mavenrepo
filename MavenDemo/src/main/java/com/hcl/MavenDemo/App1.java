dfb129199c4246006919ed04998d03662d684798
pavankumar <pavankarnati24@gmail.com>
2021-04-14 10:13:44 +0530

    created new App1

---
+++ MavenDemo/src/main/java/com/hcl/MavenDemo/App1.java
@@ -0,0 +1,17 @@
+package com.hcl.MavenDemo;
+
+import com.hcl.MavenDemo.maths.MathUtil;
+
+/**
+ * Hello world!
+ *
+ */
+public class App1 
+{
+    public static void m1( )
+    {
+        System.out.println( "m1 call" );
+        System.out.println("uploading to git...");
+        
+    }
+}
